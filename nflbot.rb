83ca8f32a253e6bbd8f6943a174668c2de8be0ac
Pat George <pat.george@gmail.com>
2019-07-25 12:45:33 +0100

    Refactored fact code

--- nflbot.rb
+++ nflbot.rb
@@ -18,36 +18,27 @@ class NFLBot < SlackRubyBot::Bot
   end
 
   match /fact about my team/ do |client, data, match|
-    say_fact(client, data['user'], data.channel)
+    team = get_team(data['user'])
+    facts = find_facts(team)
+    say_fact(client, team, facts, data.channel) if facts
   end
 
   match /fact about (the )?(.*)/ do |client, data, match|
-    say_fact_for_team(client, match[2], data.channel) if match[2]
-    # say_fact(client, match, data.channel)
+    if team = match[2]
+      facts = find_facts(team)
+      if facts
+        say_fact(client, team, facts, data.channel)
+      else
+        client.say(text: "I don't have any facts about the *#{team}* :cry:", channel: data.channel)
+      end
+    end
   end
 
   private
 
   class << self
     def get_team(slack_user_id)
-      find_team_by_slack_user_id(slack_user_id) || assign_team(slack_user_id)
-    end
-
-    def say_fact(client, slack_user_id, channel)
-      if team = find_team_by_slack_user_id(slack_user_id)
-        team = team.first
-        if facts = find_facts(team)
-          client.say(text: "Here's a fun fact about the *#{team}*: #{facts.sample}", channel: channel)
-        end
-      else
-        client.say(text: "It doesn't look like you have a team!", channel: channel)
-      end
-    end
-
-    def say_fact_for_team(client, team_name, channel)
-      if facts = find_facts(team_name)
-        client.say(text: "Here's a fun fact about the *#{team_name}*: #{facts.sample}", channel: channel)
-      end
+      (find_team_by_slack_user_id(slack_user_id) || assign_team(slack_user_id)).first
     end
 
     def find_team_by_slack_user_id(slack_user_id)
@@ -80,6 +71,10 @@ class NFLBot < SlackRubyBot::Bot
       SQL
 
       rows.first
+    end
+
+    def say_fact(client, team, facts, channel)
+      client.say(text: "Here's a fun fact about the *#{team}*: #{facts.sample}", channel: channel)
     end
 
     def database

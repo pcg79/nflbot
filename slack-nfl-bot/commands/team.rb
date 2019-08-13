4c3848e7cd2c8b028019b0c581bd9655005f5147
Pat George <patrick.george@freeagent.com>
2019-08-13 16:53:19 +0100

    Migrate to using Team model

--- slack-nfl-bot/commands/team.rb
+++ slack-nfl-bot/commands/team.rb
@@ -5,43 +5,10 @@ module SlackNFLBot
     class Team < SlackRubyBot::Commands::Base
 
       command(/(what(’|')s|which is) my team/, /(what|which) team((’|')s| is) mine/) do |client, data, match|
-        team = get_team(data.user)
+        team = ::Team.get_team(data.user)
         client.say(text: "<@#{data.user}>, your team is the *#{team}*", channel: data.channel)
       end
 
-      private
-
-      class << self
-        def get_team(slack_user_id)
-          (find_team_by_slack_user_id(slack_user_id) || assign_team(slack_user_id)).first
-        end
-
-        def find_team_by_slack_user_id(slack_user_id)
-          rows = database.execute <<-SQL
-            select name from teams t join employees_teams et
-              on t.id = et.team_id
-              where et.slack_user_id = '#{slack_user_id}'
-          SQL
-
-          rows.first
-        end
-
-        def assign_team(slack_user_id)
-          team_id = rand(32)
-          database.execute "insert into employees_teams(slack_user_id, team_id)
-              values( ?, ? )", [slack_user_id, team_id]
-
-          rows = database.execute <<-SQL
-            select name from teams where id = #{team_id}
-          SQL
-
-          rows.first
-        end
-
-        def database
-          SlackNFLBot::Database.database
-        end
-      end
     end
   end
 end
--- spec/slack-nfl-bot/commands/team_spec.rb
+++ spec/slack-nfl-bot/commands/team_spec.rb
@@ -6,14 +6,14 @@ describe SlackNFLBot::Commands::Team, vcr: { cassette_name: 'team_commands' } do
   end
 
   it "assigns a team if none assigned" do
-    expect(SlackNFLBot::Commands::Team).to receive(:assign_team).and_return(["Washington Redskins"])
+    expect(::Team).to receive(:assign_team).and_return(["Washington Redskins"])
 
     slack_message = "<@user>, your team is the *Washington Redskins*"
     expect(message: "nflbot what's my team", channel: 'channel').to respond_with_slack_message(slack_message)
   end
 
   it "returns a team if already assigned" do
-    expect(SlackNFLBot::Commands::Team).to receive(:find_team_by_slack_user_id).and_return(["Washington Redskins"])
+    expect(::Team).to receive(:find_team_by_slack_user_id).and_return(["Washington Redskins"])
 
     slack_message = "<@user>, your team is the *Washington Redskins*"
     expect(message: "nflbot what's my team", channel: 'channel').to respond_with_slack_message(slack_message)

34782763ae7894067460d9da9c0e9ca0d3c1f801
Pat George <patrick.george@freeagent.com>
2019-08-15 19:07:27 +0100

    Added test cmd for team ending in ?

--- slack-nfl-bot/commands/team.rb
+++ slack-nfl-bot/commands/team.rb
@@ -4,7 +4,7 @@ module SlackNFLBot
   module Commands
     class Team < SlackRubyBot::Commands::Base
 
-      command(/(what(’|')s|which is) my team/, /(what|which) team((’|')s| is) mine/) do |client, data, match|
+      command(/(what(’|')s|which is) my team.*/, /(what|which) team((’|')s| is) mine.*/) do |client, data, match|
         team = ::Team.get_team(slack_user_id(data))
         client.say(text: "<@#{data.user}>, your team is the *#{team}*", channel: data.channel)
       end
--- spec/slack-nfl-bot/commands/team_spec.rb
+++ spec/slack-nfl-bot/commands/team_spec.rb
@@ -5,11 +5,20 @@ describe SlackNFLBot::Commands::Team, vcr: { cassette_name: 'team_commands' } do
     SlackNFLBot::App.new
   end
 
-  it "assigns a random team if none assigned" do
-    expect(SlackNFLBot::Commands::Team).to receive(:slack_user_id).and_return(1)
+  context "assigns a random team if none assigned" do
+    it "works with 'what's my team'" do
+      expect(SlackNFLBot::Commands::Team).to receive(:slack_user_id).and_return(1)
 
-    slack_message = "<@user>, your team is the *Washington Redskins*"
-    expect(message: "nflbot what's my team", channel: 'channel').to respond_with_slack_message(slack_message)
+      slack_message = "<@user>, your team is the *Washington Redskins*"
+      expect(message: "nflbot what's my team", channel: 'channel').to respond_with_slack_message(slack_message)
+    end
+
+    it "works with 'what's my team?'" do
+      expect(SlackNFLBot::Commands::Team).to receive(:slack_user_id).and_return(1)
+
+      slack_message = "<@user>, your team is the *Washington Redskins*"
+      expect(message: "nflbot what's my team?", channel: 'channel').to respond_with_slack_message(slack_message)
+    end
   end
 
   it "returns a team if already assigned" do

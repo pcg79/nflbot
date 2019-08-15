8da46ec139263c434241476587d4f2a05cce8116
Pat George <patrick.george@freeagent.com>
2019-08-15 20:02:38 +0100

    Added fact tests and fixed regex

--- slack-nfl-bot/commands/fact.rb
+++ slack-nfl-bot/commands/fact.rb
@@ -8,7 +8,7 @@ module SlackNFLBot
         ::Fact.say_fact(client, team, fact, data.channel)
       end
 
-      match /(what(’|')s| is) a fact about (the )?(.*)\?/ do |client, data, match|
+      match /(what(’|')s| is) a fact about (the )?([\w\s]+)/ do |client, data, match|
         if team = match[4]
           fact = ::Fact.find_fact(team)
           ::Fact.say_fact(client, team, fact, data.channel)
--- spec/slack-nfl-bot/commands/fact_spec.rb
+++ spec/slack-nfl-bot/commands/fact_spec.rb
@@ -5,21 +5,37 @@ describe SlackNFLBot::Commands::Fact, vcr: { cassette_name: 'fact_commands' } do
     SlackNFLBot::App.new
   end
 
-  it "returns a fact about your assigned team" do
-    expect(::Team).to receive(:find_team_by_slack_user_id).and_return("Washington Redskins")
+  context "it returns a fact about your assigned team" do
+    it "with a question mark" do
+      expect(::Team).to receive(:find_team_by_slack_user_id).and_return("Washington Redskins")
 
-    slack_message = "Here's a fun fact about the *Washington Redskins*: Redskins fact!"
-    expect(message: "nflbot what is a fact about my team", channel: 'channel').to respond_with_slack_message(slack_message)
+      slack_message = "Here's a fun fact about the *Washington Redskins*: Redskins fact!"
+      expect(message: "nflbot what is a fact about my team?", channel: 'channel').to respond_with_slack_message(slack_message)
+    end
+
+    it "without a question mark" do
+      expect(::Team).to receive(:find_team_by_slack_user_id).and_return("Washington Redskins")
+
+      slack_message = "Here's a fun fact about the *Washington Redskins*: Redskins fact!"
+      expect(message: "nflbot what is a fact about my team", channel: 'channel').to respond_with_slack_message(slack_message)
+    end
   end
 
-  it "returns a fact about a specified team" do
-    slack_message = "Here's a fun fact about the *Washington Redskins*: Redskins fact!"
-    expect(message: "nflbot what is a fact about the Washington Redskins?", channel: 'channel').to respond_with_slack_message(slack_message)
+  context "returns a fact about a specified team" do
+    it "with a question mark" do
+      slack_message = "Here's a fun fact about the *Washington Redskins*: Redskins fact!"
+      expect(message: "nflbot what is a fact about the Washington Redskins?", channel: 'channel').to respond_with_slack_message(slack_message)
+    end
+
+    it "without a question mark" do
+      slack_message = "Here's a fun fact about the *Washington Redskins*: Redskins fact!"
+      expect(message: "nflbot what is a fact about the Washington Redskins", channel: 'channel').to respond_with_slack_message(slack_message)
+    end
   end
 
   it "states when it can't find a fact for a team" do
     slack_message = "I don't have any facts about the *New England Patriots* :cry:"
-    expect(message: "nflbot what is a fact about the New England Patriots?", channel: 'channel').to respond_with_slack_message(slack_message)
+    expect(message: "nflbot what is a fact about the New England Patriots", channel: 'channel').to respond_with_slack_message(slack_message)
   end
 
 end

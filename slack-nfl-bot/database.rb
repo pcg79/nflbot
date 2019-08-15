6372303c1e6c26549cddece954fd82b76ffeb315
Pat George <patrick.george@freeagent.com>
2019-08-15 19:00:58 +0100

    Use heroku database url

--- slack-nfl-bot/database.rb
+++ slack-nfl-bot/database.rb
@@ -7,7 +7,11 @@ module SlackNFLBot
     def self.database
       # SQLite3::Database.new "db/#{ENV['RACK_ENV']}.db"
       # PG.connect( dbname: "nflbot_#{ENV['RACK_ENV']}" )
-      Sequel.connect(adapter: :postgres, database: "nflbot_#{ENV['RACK_ENV']}")
+      if ENV['DATABASE_URL']
+        Sequel.connect(ENV['DATABASE_URL'])
+      else
+        Sequel.connect(adapter: :postgres, database: "nflbot_#{ENV['RACK_ENV']}")
+      end
     end
   end
 end

a1adc87058491a3c376305357f5f5559613f88a3
Pat George <patrick.george@freeagent.com>
2019-08-16 20:25:55 +0100

    Switched to a better nfl score feed

--- slack-nfl-bot/models/week.rb
+++ slack-nfl-bot/models/week.rb
@@ -18,17 +18,37 @@ class Week < Base
   private
 
   def self.scores_url
-    "http://www.nfl.com/liveupdate/scorestrip/ss.xml"
-  end
-
-  def week_number
-    xml_data.find_first("/ss/gms").attributes["w"]
+    "http://www.nfl.com/feeds-rs/scores"
   end
 
   def parse_games
     [].tap do |games|
-      xml_data.find("/ss/gms/g").each do |game_element|
-        games << Game.new(week_number, game_element)
+      xml_data.find("/scoresFeed/gameScores/gameScore").each do |game_score_element|
+        game_data_attrs = game_score_element.find_first("gameSchedule").attributes
+        game_params = {
+          week: game_data_attrs["week"],
+          home_team: game_data_attrs["homeDisplayName"],
+          away_team: game_data_attrs["visitorDisplayName"],
+          game_day: game_data_attrs["gameDate"],
+          game_time: game_data_attrs["isoTime"],
+        }
+
+        # /score only exists if the game has started
+        if score_element = game_score_element.find_first("score")
+          score_element_attrs = score_element.attributes
+          home_team_attrs = score_element.find_first("homeTeamScore").attributes
+          away_team_attrs = score_element.find_first("visitorTeamScore").attributes
+
+          score_params = {
+            status: score_element_attrs["phase"],
+            home_team_score: home_team_attrs["pointTotal"],
+            away_team_score: away_team_attrs["pointTotal"],
+          }
+
+          game_params.merge! score_params
+        end
+
+        games << Game.new(game_params)
       end
     end
   end

1bdec29fa2587420b986cd89737ad4caa8f2ce6c
Pat George <patrick.george@freeagent.com>
2019-08-19 11:35:36 +0100

    Added more facts

--- db/seed.rb
+++ db/seed.rb
@@ -58,6 +58,9 @@ end; nil
 {
   "Arizona Cardinals" => [
     "The Arizona Cardinals had the longest postseason victory drought in NFL history (1947 to 1998).",
+    "The Arizona Cardinals were founded as the Morgan Athletic Club in 1898, and are the oldest continuously run professional football team in the United States.",
+    "The Arizona Cardinals joined the NFL as a charter member on September 17, 1920.",
+    "The Arizona Cardinals (then in Chicago) moved to St. Louis, Missouri in 1960 before moving to Phoenix, Arizona in 1988",
   ],
   "Atlanta Falcons" => [
     "The Atlanta Falcons were born in 1965 as an NFL expansion team, which the league awarded to Atlanta-based insurance executive Rankin Smith for $8.5 million. At the time, this was the highest sum that had ever been paid for a professional sports franchise. (By comparison, in 2008 the Miami Dolphins were purchased for $1 billion. How times have changed.)",
@@ -65,12 +68,20 @@ end; nil
   ],
   "Baltimore Ravens" => [
     "In 1984 the Baltimore Colts moved to Indianapolis and became the Indianapolis Colts. Then in 1996 the Cleveland Browns moved to Baltimore to become the Baltimore Ravens. Finally, a new Cleveland Browns team debuted in 1999.",
+    "The Baltimore Ravens' name was inspired by the poem titled The Raven, by Edgar Allan Poe who spent the early part of his career in Baltimore and is buried there.",
+    "Michael Oher, whose story was made famous by the book and movie The Blind Side, was drafted by the Baltimore Ravens in 2009.",
   ],
   "Buffalo Bills" => [
     "The Bills have the distinction of being the only New York team that actually plays in New York. The Giants and Jets play at MetLife Stadium in New Jersey.",
+    "Ralph C. Wilson Jr. was awarded an AFL franchise on October 28th, 1959. The Buffalo Bills began playing competitively in 1960 with the American Football League and joined the NFL in 1970 when the AFL and NFL merger took place.",
+    "The Buffalo Bills are the only NFL team to lose four consecutive Super Bowl games.",
   ],
   "Carolina Panthers" => [
     "The nicknames “Cobras,” “Rhinos,” and “Cougars” were all considered before franchise owner Jerry Richardson settled on “Panthers” at the suggestion of his son Mark, who’d always liked the big, black cats.",
+    "The Carolina Panthers were established in 1993 by Jerry Richardson, only the second former professional football player to own an NFL franchise team.",
+    "Pro football was first introduced to the North Carolina long before the Panthers came along. A depression-era team known as the Charlotte Bantams played there for three seasons before folding in 1934. This was followed in 1941 with the Dixie League’s Charlotte Clippers, and in the 1970s when the Charlotte Hornets played for the now forgotten World Football League.",
+    "Carolina's mascot is called Sir Purr.",
+    "In 2014, the Carolina Panthers became the first team to ever win back-to-back NFC South titles",
   ],
   "Chicago Bears" => [
     "The NFL and the Chicago Bears were named on the same day. Both changed on June 24, 1922, the NFL from the American Professional Football Association and the Bears from the Chicago Staleys.",
@@ -85,9 +96,16 @@ end; nil
   ],
   "Cleveland Browns" => [
     "In 1984 the Baltimore Colts moved to Indianapolis and became the Indianapolis Colts. In 1996 the Cleveland Browns moved to Baltimore to become the Baltimore Ravens. A new Cleveland Browns team debuted in 1999.",
+    "The Cleveland Browns are the only National Football League team without a helmet logo. The logoless helmet serves as the Browns' official logo.",
+    "Between 1996 and 1998, the Cleveland Browns suspended all its operation as the owner of that time, Art Model, thought of shifting the team to Baltimore. The intellectual property of the team was kept in trust. The team resumed playing in NFL in the year 1999.",
+    "The Cleveland Browns were founded in 1945 by businessman Arthur B. McBride and coach Paul Brown as a charter member of the All-America Football Conference (AAFC). The Browns dominated the AAFC, compiling a 47–4–3 record in the league's four seasons and winning its championship in each.",
+    "When the All-America Football League folded after the 1949 season, the Cleveland Browns joined the National Football League along with the San Francisco 49ers and the original Baltimore Colts.",
   ],
   "Dallas Cowboys" => [
-    "Emmitt Smith ran for more than 18,000 yards in his 15-year career, more than any other player in history. After his retirement, he went on to win the third Dancing with the Stars competition.",
+    "Dallas Cowboy Running Back Emmitt Smith ran for more than 18,000 yards in his 15-year career, more than any other player in history. After his retirement, he went on to win the third Dancing with the Stars competition.",
+    "The Dallas Cowboys are often called 'America's Team' but we all know that's bollox.",
+    "The Dallas Cowboys were established in 1960 as an expansion team.",
+    "The 1960 Dallas Cowboys finished their inaugural campaign 0-11-1.",
   ],
   "Denver Broncos" => [
     "The Denver Broncos team mascots are Thunder II (a live horse) and Miles (the costume).",
@@ -115,10 +133,14 @@ end; nil
     "After posting a 4–12 record in their inaugural 1995 season, the Jaguars went 9–7 and earned a spot in the AFC playoffs the following season behind the standout play of quarterback Mark Brunell and wide receiver Jimmy Smith.",
   ],
   "Kansas City Chiefs" => [
-    "The Chiefs have an actual horse mascot named Warpaint."
+    "The Kansas City Chiefs have an actual horse mascot named Warpaint.",
+    "The Kansas City Chiefs began play in 1960 as the Dallas Texans. The team was a charter member of the American Football League (AFL).",
+    "The Kansas City Chiefs won the AFL Championship in 1962 as the Texas, then the team relocated to Kansas City, Missouri the following year and renamed to the Chiefs.",
   ],
   "Los Angeles Chargers" => [
-    "The Los Angeles Chargers’ existence began in the AFL back in 1960. The team moved to San Diego the very next season, and then in 2016 moved back to Los Angeles after a 56-year hiatus."
+    "The Los Angeles Chargers’ existence began in the AFL back in 1960. The team moved to San Diego the very next season, and then in 2016 moved back to Los Angeles after a 56-year hiatus.",
+    "The Los Angeles Chargers' original owner was hotel heir Barron Hilton, son of Hilton Hotels founder Conrad Hilton.",
+    "The Chargers team name was picked because the owner liked when the crowd yelled ‘charge’ and sounded a bugle at Dodger Stadium and at USC (college footbal) games.",
   ],
   "Los Angeles Rams" => [
     "The Los Angeles Rams were established in Cleveland as the Cleveland Rams and played there from 1936 to 1945.",
@@ -130,7 +152,10 @@ end; nil
     "The Dolphins are the only team in NFL history to go undefeated through the regular season and the postseason. It happened back in 1972, but don’t worry — Dolphins fans are still more than happy to brag about it.",
   ],
   "Minnesota Vikings" => [
-    "During the 1998 season the Vikings led by Randall Cunningham, Robert Smith, Cris Carter, and rookie Randy Moss, never scored less than 24 points in any of their games (that's a lot of points).",
+    "During the 1998 season the Minnesota Vikings led by Randall Cunningham, Robert Smith, Cris Carter, and rookie Randy Moss, never scored less than 24 points in any of their games (that's a lot of points).",
+    "In 1971, Minnesota Vikings Defensive Lineman Alan Page won the first ever NFL Defensive Player of the Year Award.",
+    "In a 1997 game against the Carolina Panthers, Minnesota Vikings quarterback Brad Johnson became the first NFL quarterback to ever throw a touchdown pass to himself.  He did this after catching his own deflected pass and running three yards into the end zone.",
+    "Introduced around the time the team was founded in 1961, 'Skol, Vikings' is the fight song of the Minnesota Vikings.",
   ],
   "New England Patriots" => [
     "The Patriots have won six Super Bowls in team history — most in the NFL — and they’ve won every single one of them in the Tom Brady-Bill Belichick era.",
@@ -140,6 +165,23 @@ end; nil
     "The New Orleans Saints were founded in 1967 by John W. Mecom Jr. and David Dixon.",
     "It took the New Orleans Saints 32 years to win their first playoff game.",
     "In August 2005 Hurricane Katrina caused extensive damage to the Saints home stadium - the Superdome. The Saints had to move the rest of their games to other stadiums. The Superdome was used for emergency shelter during the hurricane.",
+  ],
+  "New York Giants" => [
+    "The New York Giants were founded on August 1, 1925 and went 8-4 in their first season.",
+    "The New York Giants played their first three decades at the Polo Grounds in upper Manhattan",
+    "Their official name is still the New York Football Giants",
+    "In 1937, the Giants changed their name to the 'New York Football Giants' to distinguish themselves from the New York Giants baseball team (now San Francisco Giants), which played its home games at Manhattan's Polo Grounds at the same time.",
+    "The New York Giant's rivalry with the Philadelphia Eagles is the oldest of the NFC East rivalries, dating all the way back to 1933, and has been called the best rivalry in the NFL in the 21st century"
+  ],
+  "New York Jets" => [
+    "The New York Jets were founded in 1959 as the Titans of New York, an original member of the American Football League (AFL); later, the franchise joined the NFL in the AFL–NFL merger in 1970. The team began to play in 1960 at the Polo Grounds.",
+    "The New York Jets were originally named the Titans because titans are 'bigger and better than [the existing in-state rival New York] Giants.'",
+    "After the New York Jets were sold in 1963, the current name was adopted and the franchise moved to Shea Stadium in 1964 and then to the Meadowlands Sports Complex in 1984.",
+  ],
+  "Oakland Raiders" => [
+    "The Oakland Raiders were founded on January 30, 1960. They played their first regular season game on September 11, 1960, as a charter member of the American Football League (AFL) which merged with the NFL in 1970.",
+    "The Oakland Raiders departed Oakland to play in Los Angeles from the 1982 season until the 1994 season before returning to Oakland at the start of the 1995 season.",
+    "On March 27, 2017, NFL team owners voted nearly unanimously to approve the Raiders' application to relocate from Oakland to Las Vegas, Nevada in 2020.",
   ],
   "Philadelphia Eagles" => [
     "The Philadelphia Eagles joined the National Football League in 1933.",
@@ -153,7 +195,8 @@ end; nil
   ],
   "San Francisco 49ers" => [
     "The San Francisco 49ers were founded, and joined the NFL, in 1949.",
-    ""
+    "The San Francisco 49ers are called that because of San Francisco's history in the Gold Rush of 1849.",
+    "The San Francisco 49ers were the first team to win five Super Bowls.",
   ],
   "Seattle Seahawks" => [
     "The Seattle Seahawks were originally owned by the Nordstrom family — yes, the same family who owned the upscale department store. When the Seahawks won the Super Bowl in 2014, the team classily presented John Nordstrom with a Super Bowl ring.",
@@ -168,7 +211,9 @@ end; nil
     "The Tennessee Titans moved from Texas to Tennessee in 1997 and played in Memphis for one season before moving to Nashville. For two seasons, the team was known as the Tennessee Oilers before changing its name to the Titans in 1999.",
   ],
   "Washington Redskins" => [
-    "Art Monk held the single-season reception record (106) for eight years. Since it was broken in 1992, it has been surpassed more than 40 times.",
+    "Washinton Redskins wide receiver Art Monk held the single-season reception record (106) for eight years. Since it was broken in 1992, it has been surpassed more than 40 times.",
+    "The Washington Redskins were the first team in the NFL with an official marching band, and also the first team to have a fight song, 'Hail to the Redskins'.",
+    "The team began play as the Boston Braves in 1932, based in Boston, before relocating to Washington, D.C. in 1937.",
   ]
 }.each do |name, facts_array|
   facts_array.each do |fact|

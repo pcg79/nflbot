57990e507a8cdc12e30b8c6d26085b78895185be
Pat George <patrick.george@freeagent.com>
2019-08-21 11:39:06 +0100

    Added more help text

--- slack-nfl-bot/commands/help.rb
+++ slack-nfl-bot/commands/help.rb
@@ -7,6 +7,8 @@ module SlackNFLBot
 
         *what's my team* (and variations) - Tells you your team if you have one, assigns you one if you don't.
         *how'd my team do* (and variations) - Tells you how your team did in the latest game.
+        *what's a fact about my team* (and variations) - Tells you a fun fact about your assigned team.
+        *what's a fact about [full team name]* (and variations) - Tells you a fun fact about [full team name].
         *scores* - Recap of the week including scores and upcoming games for the week.
         *standings* - Current NFL standings by division
 

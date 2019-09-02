e731f43b3cda13b18a1e8527d47957b08b983ec9
Pat George <patrick.george@freeagent.com>
2019-09-02 19:12:46 +0100

    Implemented assignment list

---
+++ slack-nfl-bot/models/employee_team.rb
@@ -0,0 +1,16 @@
+class EmployeeTeam < Base
+
+  class << self
+
+    def employees_teams_group_by_team
+      grouped_hashes = employees_teams.join(:teams, id: :team_id).select(:slack_user_id, :name).all.group_by { |h| h[:name] }
+
+      grouped_hashes.each { |k, array| grouped_hashes[k] = array.map { |hash| hash[:slack_user_id] } }
+    end
+
+    def employees_teams
+      database[:employees_teams]
+    end
+
+  end
+end
